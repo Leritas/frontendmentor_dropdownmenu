44fb0a4df8f5b782196f3c1dd9422f6a45d57d6b
leritas <din.fuso@mail.ru>
2022-05-11 03:20:15 +0300

    ok i promise this is last commit on code)

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const nav = document.querySelector('nav.mobile')
 const captions = document.querySelectorAll('.dropdown-caption')
 const darkBg = document.querySelector('.dark-background')
+const menu = document.querySelectorAll('.menu')
+
+
+for (let el of menu) {
+    el.addEventListener('click', () => {
+        nav.classList.toggle('active')
+        darkBg.classList.toggle('active')
+    })
+}
+
 for (let elem of captions) {
     elem.addEventListener('click', () => {
         elem.parentElement.classList.toggle('dd-show')
     })
-}
-
-function menu() {
-    nav.classList.toggle('active')
-    darkBg.classList.toggle('active')
 }
